cd8356a767045b1fd292d5fc9b1c8e6cb2b4e6f0
Aaron Hurt <ahurt@ena.com>
2017-06-21 16:16:54 -0500

    no need to pre-create path

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ ln -sf /tmp/build/SPECS/consul.spec $HOME/rpmbuild/SPECS/
 find /tmp/build/SOURCES -type f -exec ln -sf {} $HOME/rpmbuild/SOURCES \;
 spectool -g -R $HOME/rpmbuild/SPECS/consul.spec
 rpmbuild -ba $HOME/rpmbuild/SPECS/consul.spec
-mkdir -p /tmp/artifacts
 sudo cp -f $HOME/rpmbuild/RPMS/x86_64/consul*.rpm /tmp/artifacts/
