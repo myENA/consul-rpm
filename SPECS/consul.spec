de5539766a23e9034878d6db18768f30ed0a5320
Aaron Hurt <ahurt@ena.com>
2017-07-06 10:33:09 -0500

    build script updates and upstream update to 0.8.5

--- SPECS/consul.spec
+++ SPECS/consul.spec
@@ -9,8 +9,8 @@
 %define check_docker_dist https://github.com/newrelic/check_docker/releases/download/v2.3/check_docker-linux-2.3
 
 Name:           consul
-Version:        0.8.4
-Release:        1%{?dist}
+Version:        0.8.5
+Release:        0%{?dist}
 Summary:        Service discovery and configuration made easy.
 
 Group:          System Environment/Daemons
--- build.sh
+++ build.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 set -ex  ## we like things verbose
-## install helpful tools
-sudo yum install -y rpmdevtools rpm-devel rpm-build mock
+
+## ensure we have a full dev stack
+sudo yum groupinstall -y "Development Tools"
+
+## install other helpers
+sudo yum install -y epel-release mock rpmdevtools rpm-devel
+
 ## setup our build path
 rpmdev-setuptree
+
 ## link the specs
-ln -sf /tmp/build/SPECS/consul.spec $HOME/rpmbuild/SPECS/
+ln -sf /tmp/build/SPECS/*.spec $HOME/rpmbuild/SPECS/
+
 ## link the sources
 find /tmp/build/SOURCES -type f -exec ln -sf {} $HOME/rpmbuild/SOURCES/ \;
+
 ## download sources
-spectool -g -R $HOME/rpmbuild/SPECS/consul.spec
-## build our packages
-rpmbuild -ba $HOME/rpmbuild/SPECS/consul.spec
+for spec in $HOME/rpmbuild/SPECS/*.spec; do
+    spectool -g -R $spec
+done
+
+## build packages
+rpmbuild -ba $HOME/rpmbuild/SPECS/*.spec
+
 ## copy built files out of the vagrant/docker environment
 ## skip if you are doing this manually
 if [ -f /.dockerenv ] || [ -f /.doing_the_vagrant ]; then
-    sudo cp -f $HOME/rpmbuild/RPMS/x86_64/consul*.rpm /tmp/artifacts/
+    sudo cp -f $HOME/rpmbuild/RPMS/*/*.rpm /tmp/artifacts/ || true
+    sudo cp -f $HOME/rpmbuild/SRPMS/*.rpm /tmp/artifacts/ || true
 fi
